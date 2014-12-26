33e2855db034b0ad8250de45942de0fa10756271
Fabricio20 <fabricio20r@hotmail.com>
2014-12-26 13:10:05 -0200

    Added Reflection For Action API

---
+++ HubBasics/src/me/Fabricio20/API/ActionAPI.java
@@ -0,0 +1,17 @@
+package me.Fabricio20.API;
+
+public class ActionAPI {
+	
+	/*public static void sendAction(Player player, String message) {
+		if(isPlayerRightVersion(player)) {
+			IChatBaseComponent icbc = ChatSerializer.a("{\"text\": \"" + message + "\"}");
+		    PacketPlayOutChat bar = new PacketPlayOutChat(icbc, (byte)2);
+		    ((CraftPlayer)player).getHandle().playerConnection.sendPacket(bar);
+		}
+	}
+	
+	public static boolean isPlayerRightVersion(Player player) {
+		return ((CraftPlayer)player).getHandle().playerConnection.networkManager.getVersion() >= 47;
+	}
+	*/
+}

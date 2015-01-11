259b26ab824970191ca8b251537c7a882e12633d
Fabricio20 <fabricio20r@hotmail.com>
2015-01-11 18:20:52 -0200

    Fix PressurePlate Glitch

--- HubBasics/src/me/Fabricio20/listeners/RightClickListener.java
+++ HubBasics/src/me/Fabricio20/listeners/RightClickListener.java
@@ -100,66 +100,68 @@ public class RightClickListener implements Listener {
 			if(Main.theClass.config.getBoolean("Others.JoinItems") == true) {
 				List<String> worlds = Main.theClass.config.getStringList("Worlds");
 				if(worlds.contains(e.getPlayer().getWorld().getName())) {
-					if(Main.theClass.ItemConfig.getBoolean("Item1.Enabled") == true) {
-						if(e.getPlayer().getItemInHand().equals(Items.Item1(e.getPlayer().getName()))) {
-							if(Main.theClass.ItemConfig.getString("Item1.Command") != null) {
-								e.getPlayer().chat(Main.theClass.ItemConfig.getString("Item1.Command"));
+					if(e.getAction().equals(Action.RIGHT_CLICK_AIR) || e.getAction().equals(Action.RIGHT_CLICK_BLOCK)) {
+						if(Main.theClass.ItemConfig.getBoolean("Item1.Enabled") == true) {
+							if(e.getPlayer().getItemInHand().equals(Items.Item1(e.getPlayer().getName()))) {
+								if(Main.theClass.ItemConfig.getString("Item1.Command") != null) {
+									e.getPlayer().chat(Main.theClass.ItemConfig.getString("Item1.Command"));
+								}
 							}
 						}
-					}
-					if(Main.theClass.ItemConfig.getBoolean("Item2.Enabled") == true) {
-						if(e.getPlayer().getItemInHand().equals(Items.Item2(e.getPlayer().getName()))) {
-							if(Main.theClass.ItemConfig.getString("Item2.Command") != null) {
-								e.getPlayer().chat(Main.theClass.ItemConfig.getString("Item2.Command"));
+						if(Main.theClass.ItemConfig.getBoolean("Item2.Enabled") == true) {
+							if(e.getPlayer().getItemInHand().equals(Items.Item2(e.getPlayer().getName()))) {
+								if(Main.theClass.ItemConfig.getString("Item2.Command") != null) {
+									e.getPlayer().chat(Main.theClass.ItemConfig.getString("Item2.Command"));
+								}
 							}
 						}
-					}
-					if(Main.theClass.ItemConfig.getBoolean("Item3.Enabled") == true) {
-						if(e.getPlayer().getItemInHand().equals(Items.Item3(e.getPlayer().getName()))) {
-							if(Main.theClass.ItemConfig.getString("Item3.Command") != null) {
-								e.getPlayer().chat(Main.theClass.ItemConfig.getString("Item3.Command"));
+						if(Main.theClass.ItemConfig.getBoolean("Item3.Enabled") == true) {
+							if(e.getPlayer().getItemInHand().equals(Items.Item3(e.getPlayer().getName()))) {
+								if(Main.theClass.ItemConfig.getString("Item3.Command") != null) {
+									e.getPlayer().chat(Main.theClass.ItemConfig.getString("Item3.Command"));
+								}
 							}
 						}
-					}
-					if(Main.theClass.ItemConfig.getBoolean("Item4.Enabled") == true) {
-						if(e.getPlayer().getItemInHand().equals(Items.Item4(e.getPlayer().getName()))) {
-							if(Main.theClass.ItemConfig.getString("Item4.Command") != null) {
-								e.getPlayer().chat(Main.theClass.ItemConfig.getString("Item4.Command"));
+						if(Main.theClass.ItemConfig.getBoolean("Item4.Enabled") == true) {
+							if(e.getPlayer().getItemInHand().equals(Items.Item4(e.getPlayer().getName()))) {
+								if(Main.theClass.ItemConfig.getString("Item4.Command") != null) {
+									e.getPlayer().chat(Main.theClass.ItemConfig.getString("Item4.Command"));
+								}
 							}
 						}
-					}
-					if(Main.theClass.ItemConfig.getBoolean("Item5.Enabled") == true) {
-						if(e.getPlayer().getItemInHand().equals(Items.Item5(e.getPlayer().getName()))) {
-							if(Main.theClass.ItemConfig.getString("Item5.Command") != null) {
-								e.getPlayer().chat(Main.theClass.ItemConfig.getString("Item5.Command"));
+						if(Main.theClass.ItemConfig.getBoolean("Item5.Enabled") == true) {
+							if(e.getPlayer().getItemInHand().equals(Items.Item5(e.getPlayer().getName()))) {
+								if(Main.theClass.ItemConfig.getString("Item5.Command") != null) {
+									e.getPlayer().chat(Main.theClass.ItemConfig.getString("Item5.Command"));
+								}
 							}
 						}
-					}
-					if(Main.theClass.ItemConfig.getBoolean("Item6.Enabled") == true) {
-						if(e.getPlayer().getItemInHand().equals(Items.Item6(e.getPlayer().getName()))) {
-							if(Main.theClass.ItemConfig.getString("Item6.Command") != null) {
-								e.getPlayer().chat(Main.theClass.ItemConfig.getString("Item6.Command"));
+						if(Main.theClass.ItemConfig.getBoolean("Item6.Enabled") == true) {
+							if(e.getPlayer().getItemInHand().equals(Items.Item6(e.getPlayer().getName()))) {
+								if(Main.theClass.ItemConfig.getString("Item6.Command") != null) {
+									e.getPlayer().chat(Main.theClass.ItemConfig.getString("Item6.Command"));
+								}
 							}
 						}
-					}
-					if(Main.theClass.ItemConfig.getBoolean("Item7.Enabled") == true) {
-						if(e.getPlayer().getItemInHand().equals(Items.Item7(e.getPlayer().getName()))) {
-							if(Main.theClass.ItemConfig.getString("Item7.Command") != null) {
-								e.getPlayer().chat(Main.theClass.ItemConfig.getString("Item7.Command"));
+						if(Main.theClass.ItemConfig.getBoolean("Item7.Enabled") == true) {
+							if(e.getPlayer().getItemInHand().equals(Items.Item7(e.getPlayer().getName()))) {
+								if(Main.theClass.ItemConfig.getString("Item7.Command") != null) {
+									e.getPlayer().chat(Main.theClass.ItemConfig.getString("Item7.Command"));
+								}
 							}
 						}
-					}
-					if(Main.theClass.ItemConfig.getBoolean("Item8.Enabled") == true) {
-						if(e.getPlayer().getItemInHand().equals(Items.Item8(e.getPlayer().getName()))) {
-							if(Main.theClass.ItemConfig.getString("Item8.Command") != null) {
-								e.getPlayer().chat(Main.theClass.ItemConfig.getString("Item8.Command"));
+						if(Main.theClass.ItemConfig.getBoolean("Item8.Enabled") == true) {
+							if(e.getPlayer().getItemInHand().equals(Items.Item8(e.getPlayer().getName()))) {
+								if(Main.theClass.ItemConfig.getString("Item8.Command") != null) {
+									e.getPlayer().chat(Main.theClass.ItemConfig.getString("Item8.Command"));
+								}
 							}
 						}
-					}
-					if(Main.theClass.ItemConfig.getBoolean("Item9.Enabled") == true) {
-						if(e.getPlayer().getItemInHand().equals(Items.Item9(e.getPlayer().getName()))) {
-							if(Main.theClass.ItemConfig.getString("Item9.Command") != null) {
-								e.getPlayer().chat(Main.theClass.ItemConfig.getString("Item9.Command"));
+						if(Main.theClass.ItemConfig.getBoolean("Item9.Enabled") == true) {
+							if(e.getPlayer().getItemInHand().equals(Items.Item9(e.getPlayer().getName()))) {
+								if(Main.theClass.ItemConfig.getString("Item9.Command") != null) {
+									e.getPlayer().chat(Main.theClass.ItemConfig.getString("Item9.Command"));
+								}
 							}
 						}
 					}

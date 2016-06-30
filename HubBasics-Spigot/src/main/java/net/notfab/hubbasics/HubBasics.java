ccc292440256b8e3fa1d0514a42459e069b03b1d
LorgeN <eirikltanberg@gmail.com>
2016-06-30 04:16:15 +0200

    Header update

--- HubBasics-Spigot/src/main/java/net/notfab/hubbasics/HubBasics.java
+++ HubBasics-Spigot/src/main/java/net/notfab/hubbasics/HubBasics.java
@@ -6,6 +6,16 @@ import net.notfab.hubbasics.settings.HConfiguration;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+/*
+ * Copyright (c) 2016.
+ *
+ * The contents of this project are licensed under a Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International.
+ * Please read the information linked below before you attempt to use this project or it's contents to make sure you are abiding
+ * by it's terms.
+ *
+ * https://creativecommons.org/licenses/by-nc-sa/4.0/
+ */
+
 public class HubBasics extends JavaPlugin {
 
     @Getter private static HubBasics Instance;

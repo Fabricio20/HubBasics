4b9a72656935c07b2744693cf2874e9aee4fa72e
Fabricio20 <fabricio20r@hotmail.com>
2016-07-02 14:23:11 -0300

    Register Command

--- HubBasics-Spigot/src/main/java/net/notfab/hubbasics/managers/CommandManager.java
+++ HubBasics-Spigot/src/main/java/net/notfab/hubbasics/managers/CommandManager.java
@@ -2,22 +2,12 @@
 package net.notfab.hubbasics.managers;
 
 import lombok.Getter;
-import lombok.Setter;
-
 import net.notfab.hubbasics.HubBasics;
 import net.notfab.hubbasics.abstracts.command.HCommand;
-import net.notfab.hubbasics.plugin.messages.HMessenger;
-
-import org.bukkit.Bukkit;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandMap;
-import org.bukkit.command.CommandSender;
+import net.notfab.hubbasics.commands.HatCommand;
 import org.bukkit.command.PluginCommand;
-import org.bukkit.command.TabCompleter;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
@@ -39,6 +29,7 @@ public class CommandManager {
 
     public CommandManager() {
         this.commands = new ArrayList<>();
+        registerCommand(new HatCommand(), HubBasics.getInstance());
     }
 
     /**

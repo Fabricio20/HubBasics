14115294631eb358a197525f808a1cb53529a51b
Fabricio20 <fabricio20r@hotmail.com>
2017-02-05 08:22:52 +0900

    Ignore casing on world name

--- HubBasics-Spigot/src/main/java/net/notfab/hubbasics/modules/JoinTeleport.java
+++ HubBasics-Spigot/src/main/java/net/notfab/hubbasics/modules/JoinTeleport.java
@@ -10,8 +10,10 @@ package net.notfab.hubbasics.modules;
  * https://creativecommons.org/licenses/by-nc-sa/4.0/
  */
 
+import net.notfab.hubbasics.HubBasics;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerJoinEvent;
 
@@ -36,9 +38,17 @@ public class JoinTeleport extends Module {
         double z = getDouble(ConfigurationKey.JOIN_TELEPORT_LOCATION_Z);
         float yaw = getDouble(ConfigurationKey.JOIN_TELEPORT_LOCATION_YAW).floatValue();
         float pitch = getDouble(ConfigurationKey.JOIN_TELEPORT_LOCATION_PITCH).floatValue();
-        String world = getString(ConfigurationKey.JOIN_TELEPORT_LOCATION_WORLD);
-        if (!Bukkit.getWorlds().contains(world))
+        String world = null;
+        for(World w: Bukkit.getWorlds()) {
+            if(w.getName().equalsIgnoreCase(getString(ConfigurationKey.JOIN_TELEPORT_LOCATION_WORLD))) {
+                world = w.getName();
+            }
+        }
+        if(world == null) {
             Bukkit.getConsoleSender().sendMessage(ChatColor.RED + "YOU HAVE SPECIFIED A WORLD THAT DOES NOT EXIST IN THE CONFIGURATION FILE FOR JOIN_TELEPORT!");
+            HubBasics.getInstance().getServer().getScheduler().runTaskLaterAsynchronously(HubBasics.getInstance(), () -> HubBasics.getInstance().getModuleManager().getModule(ModuleEnum.JOIN_TELEPORT).onDisableInternal(), 40);
+            return;
+        }
         this.location = new Location(Bukkit.getWorld(world), x, y, z, yaw, pitch);
     }
 

44a33f9dd28e4f763f60460604754e43209b6acf
Fabricio20 <git@notfab.net>
2019-12-26 19:00:25 -0300

    Release 6.9.8
    Better Handling of Offhand and Main Hand interactions.

--- HB-Spigot/src/main/java/net/notfab/hubbasics/spigot/listeners/ItemListener.java
+++ HB-Spigot/src/main/java/net/notfab/hubbasics/spigot/listeners/ItemListener.java
@@ -11,10 +11,8 @@ import org.bukkit.event.block.Action;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Copyright (c) HubBasics 2018.
@@ -33,94 +31,102 @@ public class ItemListener implements Listener {
     @EventHandler(ignoreCancelled = true)
     public void onItemMove(InventoryClickEvent event) {
         ItemStack currentItem = event.getCurrentItem();
-        if(currentItem == null || currentItem.getType() == Material.AIR) return;
+        if (currentItem == null || currentItem.getType() == Material.AIR) return;
         NBTItem nbtItem = new NBTItem(currentItem);
-        if(!nbtItem.hasKey("HubBasics")) return;
+        if (!nbtItem.hasKey("HubBasics")) return;
 
         CustomItem item = HubBasics.getInstance().getItemManager().get(nbtItem.getString("HubBasics"));
-        if(item == null) {
+        if (item == null) {
             currentItem.setType(Material.AIR); // Destroy old item
             return;
         }
-        if(item.getRunOnInventory()) {
+        if (item.getRunOnInventory()) {
             item.onCommand((Player) event.getWhoClicked());
         }
-        if(!item.getAllowMove())
+        if (!item.getAllowMove())
             event.setCancelled(true); // Call setCancelled only when needed to not conflict with other plugins
     }
 
     @EventHandler(ignoreCancelled = true)
     public void onItemDrop(PlayerDropItemEvent event) {
         ItemStack currentItem = event.getItemDrop().getItemStack();
-        if(currentItem == null || currentItem.getType() == Material.AIR) return;
+        if (currentItem.getType() == Material.AIR) return;
         NBTItem nbtItem = new NBTItem(currentItem);
-        if(!nbtItem.hasKey("HubBasics")) return;
+        if (!nbtItem.hasKey("HubBasics")) return;
 
         CustomItem item = HubBasics.getInstance().getItemManager().get(nbtItem.getString("HubBasics"));
-        if(item == null) {
+        if (item == null) {
             currentItem.setType(Material.AIR); // Destroy old item
             return;
         }
-        if(!item.getAllowDrop())
+        if (!item.getAllowDrop())
             event.setCancelled(true); // Call setCancelled only when needed to not conflict with other plugins
     }
 
     @EventHandler(ignoreCancelled = true)
     public void onLeftClick(PlayerInteractEvent event) {
-        if(!(event.getAction() == Action.LEFT_CLICK_AIR || event.getAction() == Action.LEFT_CLICK_BLOCK)) return;
-        List<ItemStack> items = new ArrayList<>();
-        ItemStack mainHand = event.getPlayer().getInventory().getItemInMainHand();
-        if(mainHand != null) items.add(mainHand);
-        ItemStack offHand = event.getPlayer().getInventory().getItemInOffHand();
-        if(offHand != null) items.add(offHand);
-
-        items.stream().filter(item -> item.getType() != Material.AIR).forEach(itemStack -> {
+        if (!(event.getAction() == Action.LEFT_CLICK_AIR || event.getAction() == Action.LEFT_CLICK_BLOCK)) {
+            return;
+        }
+        ItemStack itemStack;
+        if (event.getHand() == EquipmentSlot.HAND) {
+            itemStack = event.getPlayer().getInventory().getItemInMainHand();
+        } else if (event.getHand() == EquipmentSlot.OFF_HAND) {
+            itemStack = event.getPlayer().getInventory().getItemInOffHand();
+        } else {
+            return;
+        }
+        if (itemStack.getType() != Material.AIR) {
             NBTItem nbtItem = new NBTItem(itemStack);
-            if(!nbtItem.hasKey("HubBasics")) return;
-
+            if (!nbtItem.hasKey("HubBasics")) return;
             event.setCancelled(true);
             CustomItem item = HubBasics.getInstance().getItemManager().get(nbtItem.getString("HubBasics"));
-            if(item == null) {
+            if (item == null) {
                 itemStack.setType(Material.AIR); // Destroy old item
                 return;
             }
-            if(!item.getRunOnLeftClick()) return;
-
-            if(mainHand == itemStack) {
-                item.onCommand(event.getPlayer());
-            } else if(offHand == itemStack && item.getRunOnOffhand()) {
+            if (!item.getRunOnLeftClick()) return;
+            if (event.getHand() == EquipmentSlot.OFF_HAND) {
+                if (item.getRunOnOffhand()) {
+                    item.onCommand(event.getPlayer());
+                }
+            } else {
                 item.onCommand(event.getPlayer());
             }
-        });
+        }
     }
 
     @EventHandler
     public void onRightClick(PlayerInteractEvent event) {
-        if(!(event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK)) return;
-        List<ItemStack> items = new ArrayList<>();
-        ItemStack mainHand = event.getPlayer().getInventory().getItemInMainHand();
-        if(mainHand != null) items.add(mainHand);
-        ItemStack offHand = event.getPlayer().getInventory().getItemInOffHand();
-        if(offHand != null) items.add(offHand);
-
-        items.stream().filter(item -> item.getType() != Material.AIR).forEach(itemStack -> {
+        if (!(event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK)) {
+            return;
+        }
+        ItemStack itemStack;
+        if (event.getHand() == EquipmentSlot.HAND) {
+            itemStack = event.getPlayer().getInventory().getItemInMainHand();
+        } else if (event.getHand() == EquipmentSlot.OFF_HAND) {
+            itemStack = event.getPlayer().getInventory().getItemInOffHand();
+        } else {
+            return;
+        }
+        if (itemStack.getType() != Material.AIR) {
             NBTItem nbtItem = new NBTItem(itemStack);
-            if(!nbtItem.hasKey("HubBasics")) return;
-
+            if (!nbtItem.hasKey("HubBasics")) return;
             event.setCancelled(true);
             CustomItem item = HubBasics.getInstance().getItemManager().get(nbtItem.getString("HubBasics"));
-            if(item == null) {
+            if (item == null) {
                 itemStack.setType(Material.AIR); // Destroy old item
                 return;
             }
-            if(!item.getRunOnRightClick()) return;
-
-            if(mainHand == itemStack) {
-                item.onCommand(event.getPlayer());
-            } else if(offHand == itemStack && item.getRunOnOffhand()) {
+            if (!item.getRunOnRightClick()) return;
+            if (event.getHand() == EquipmentSlot.OFF_HAND) {
+                if (item.getRunOnOffhand()) {
+                    item.onCommand(event.getPlayer());
+                }
+            } else {
                 item.onCommand(event.getPlayer());
             }
-        });
+        }
     }
 
 }

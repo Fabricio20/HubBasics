18dae1a320e721d28f02100cefcd9dabea3302b6
Fabricio20 <git@notfab.net>
2020-01-26 18:27:22 -0300

    Release 6.11.1
    Fixed build numbers and added placeholder replacement on Messenger.
    
    Signed-off-by: Fabricio20 <git@notfab.net>

--- Spigot/src/main/java/net/notfab/hubbasics/spigot/managers/Messenger.java
+++ Spigot/src/main/java/net/notfab/hubbasics/spigot/managers/Messenger.java
@@ -1,5 +1,6 @@
 package net.notfab.hubbasics.spigot.managers;
 
+import net.notfab.hubbasics.spigot.utils.PlaceHolderUtil;
 import org.bukkit.ChatColor;
 import org.bukkit.World;
 import org.bukkit.command.CommandSender;
@@ -10,12 +11,12 @@ public class Messenger {
 
     public void send(CommandSender sender, String... messages) {
         Arrays.asList(messages).forEach(message ->
-                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', message)));
+                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', PlaceHolderUtil.replace(sender, message))));
     }
 
     public void send(World world, String... messages) {
         Arrays.asList(messages).forEach(message -> world.getPlayers().forEach(player ->
-                player.sendMessage(ChatColor.translateAlternateColorCodes('&', message))));
+                player.sendMessage(ChatColor.translateAlternateColorCodes('&', PlaceHolderUtil.replace(player, message)))));
     }
 
 }

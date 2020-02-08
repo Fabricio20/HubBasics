fb48a7a83b1e0630829147a1a2dbb0de17ed6c16
Fabricio20 <git@notfab.net>
2020-02-08 13:57:53 -0300

    Release 6.11.2
    Remove dependency on FuseSource Ansi (PaperMC Does not have it).
    
    Signed-off-by: Fabricio20 <git@notfab.net>

--- Spigot/src/main/java/net/notfab/hubbasics/spigot/managers/HBLogger.java
+++ Spigot/src/main/java/net/notfab/hubbasics/spigot/managers/HBLogger.java
@@ -4,7 +4,6 @@ import lombok.Getter;
 import lombok.Setter;
 import net.notfab.hubbasics.spigot.entities.EnumModules;
 import org.bukkit.ChatColor;
-import org.fusesource.jansi.Ansi;
 
 import java.util.EnumMap;
 import java.util.Map;
@@ -46,7 +45,7 @@ public class HBLogger {
                 message = message.replaceAll("(?i)" + color.toString(), "");
             }
         }
-        return message + Ansi.ansi().reset().toString();
+        return message + "\u001B[m";
     }
 
     // ------------------------------------------------------------------
@@ -130,28 +129,28 @@ public class HBLogger {
     }
 
     static {
-        replacements.put(ChatColor.BLACK, Ansi.ansi().a(Ansi.Attribute.RESET).fg(Ansi.Color.BLACK).boldOff().toString());
-        replacements.put(ChatColor.DARK_BLUE, Ansi.ansi().a(Ansi.Attribute.RESET).fg(Ansi.Color.BLUE).boldOff().toString());
-        replacements.put(ChatColor.DARK_GREEN, Ansi.ansi().a(Ansi.Attribute.RESET).fg(Ansi.Color.GREEN).boldOff().toString());
-        replacements.put(ChatColor.DARK_AQUA, Ansi.ansi().a(Ansi.Attribute.RESET).fg(Ansi.Color.CYAN).boldOff().toString());
-        replacements.put(ChatColor.DARK_RED, Ansi.ansi().a(Ansi.Attribute.RESET).fg(Ansi.Color.RED).boldOff().toString());
-        replacements.put(ChatColor.DARK_PURPLE, Ansi.ansi().a(Ansi.Attribute.RESET).fg(Ansi.Color.MAGENTA).boldOff().toString());
-        replacements.put(ChatColor.GOLD, Ansi.ansi().a(Ansi.Attribute.RESET).fg(Ansi.Color.YELLOW).boldOff().toString());
-        replacements.put(ChatColor.GRAY, Ansi.ansi().a(Ansi.Attribute.RESET).fg(Ansi.Color.WHITE).boldOff().toString());
-        replacements.put(ChatColor.DARK_GRAY, Ansi.ansi().a(Ansi.Attribute.RESET).fg(Ansi.Color.BLACK).bold().toString());
-        replacements.put(ChatColor.BLUE, Ansi.ansi().a(Ansi.Attribute.RESET).fg(Ansi.Color.BLUE).bold().toString());
-        replacements.put(ChatColor.GREEN, Ansi.ansi().a(Ansi.Attribute.RESET).fg(Ansi.Color.GREEN).bold().toString());
-        replacements.put(ChatColor.AQUA, Ansi.ansi().a(Ansi.Attribute.RESET).fg(Ansi.Color.CYAN).bold().toString());
-        replacements.put(ChatColor.RED, Ansi.ansi().a(Ansi.Attribute.RESET).fg(Ansi.Color.RED).bold().toString());
-        replacements.put(ChatColor.LIGHT_PURPLE, Ansi.ansi().a(Ansi.Attribute.RESET).fg(Ansi.Color.MAGENTA).bold().toString());
-        replacements.put(ChatColor.YELLOW, Ansi.ansi().a(Ansi.Attribute.RESET).fg(Ansi.Color.YELLOW).bold().toString());
-        replacements.put(ChatColor.WHITE, Ansi.ansi().a(Ansi.Attribute.RESET).fg(Ansi.Color.WHITE).bold().toString());
-        replacements.put(ChatColor.MAGIC, Ansi.ansi().a(Ansi.Attribute.BLINK_SLOW).toString());
-        replacements.put(ChatColor.BOLD, Ansi.ansi().a(Ansi.Attribute.UNDERLINE_DOUBLE).toString());
-        replacements.put(ChatColor.STRIKETHROUGH, Ansi.ansi().a(Ansi.Attribute.STRIKETHROUGH_ON).toString());
-        replacements.put(ChatColor.UNDERLINE, Ansi.ansi().a(Ansi.Attribute.UNDERLINE).toString());
-        replacements.put(ChatColor.ITALIC, Ansi.ansi().a(Ansi.Attribute.ITALIC).toString());
-        replacements.put(ChatColor.RESET, Ansi.ansi().a(Ansi.Attribute.RESET).toString());
+        replacements.put(ChatColor.BLACK, "\u001B[0;30;22m");
+        replacements.put(ChatColor.DARK_BLUE, "\u001B[0;34;22m");
+        replacements.put(ChatColor.DARK_GREEN, "\u001B[0;32;22m");
+        replacements.put(ChatColor.DARK_AQUA, "\u001B[0;36;22m");
+        replacements.put(ChatColor.DARK_RED, "\u001B[0;31;22m");
+        replacements.put(ChatColor.DARK_PURPLE, "\u001B[0;35;22m");
+        replacements.put(ChatColor.GOLD, "\u001B[0;33;22m");
+        replacements.put(ChatColor.GRAY, "\u001B[0;37;22m");
+        replacements.put(ChatColor.DARK_GRAY, "\u001B[0;30;1m");
+        replacements.put(ChatColor.BLUE, "\u001B[0;34;1m");
+        replacements.put(ChatColor.GREEN, "\u001B[0;32;1m");
+        replacements.put(ChatColor.AQUA, "\u001B[0;36;1m");
+        replacements.put(ChatColor.RED, "\u001B[0;31;1m");
+        replacements.put(ChatColor.LIGHT_PURPLE, "\u001B[0;35;1m");
+        replacements.put(ChatColor.YELLOW, "\u001B[0;33;1m");
+        replacements.put(ChatColor.WHITE, "\u001B[0;37;1m");
+        replacements.put(ChatColor.MAGIC, "\u001B[5m");
+        replacements.put(ChatColor.BOLD, "\u001B[21m");
+        replacements.put(ChatColor.STRIKETHROUGH, "\u001B[9m");
+        replacements.put(ChatColor.UNDERLINE, "\u001B[4m");
+        replacements.put(ChatColor.ITALIC, "\u001B[3m");
+        replacements.put(ChatColor.RESET, "\u001B[m");
     }
 
 }
